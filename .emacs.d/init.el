b6ac60e31ef2a119c47ee7e69c8d9cf701017b87
Marco Rolappe <m_rolappe@gmx.net>
2021-02-05 12:49:32 +0100

    current state

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -1,4 +1,3 @@
-;; tmp dotf .emacs.d
 (require 'org)
 (require 'ob-tangle)
 
@@ -7,7 +6,7 @@
 (setq loader-org-file-name (expand-file-name "loader.org" init-dir))
 
 (if (file-exists-p loader-org-file-name)
-    (org-babel-load-file loader-org-file-name)
+  (org-babel-load-file loader-org-file-name)
   (message "loader.org not found: " loader-org-file-name))
 
 (defun rlp/edit-loader-org ()
@@ -23,20 +22,20 @@
   "find git-repos dir"
   (interactive)
   (find-file "~/git-repos"))
-
 (custom-set-variables
  ;; custom-set-variables was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(custom-safe-themes
-   '("14f13fee1792f44c448df33e3d3a03ce9adbf1b47da8be490f604ac7ae6659b9" "9a313a6a47c9655d9f2b87e846a31e9c21076f71d85bfb22065b21d5e8da684d" "0ecfd25c7f4b32b4703dfbbad3c0cb865aeb17efb0b694624a604ed89550ee8d" "9271c0ad73ef29af016032376d36e8aed4e89eff17908c0b578c33e54dfa1da1" "d543a5f82ce200d50bdce81b2ecc4db51422439ba7c0e6845483dd89566e4cf9" default))
  '(package-selected-packages
-   '(docker ivy-rich company-quickhelp eshell-git-prompt vterm kotlin-mode lsp-mode anki-editor mu4e-alert helpful forth-mode dimmer org-download rustic htmlize org-caldav rg flycheck clojure-snippets lispy restclient focus keyfreq dashboard spaceline deft rainbow-delimiters smartparens git-auto-commit-mode magit bm which-key ace-link counsel ivy-hydra ivy evil solarized-theme use-package)))
+   '(lua-mode org-bullets htmlize doom-themes doom-modeline forth-mode keepass-mode go-mode org-ql fuel geiser ox-hugo magrant keypression nov circe erc emms lispy paren-face smartparens engine-mode restclient all-the-icons-dired all-the-icons ace-window rustic mu4e-views pocket-reader counsel-ffdata haskell-mode docker ivy-rich yasnippet-snippets yasnippet eshell-git-prompt vterm lsp-ivy lsp-ui lsp-mode kotlin-mode company-quickhelp company elfeed-org elfeed flycheck anki-editor mu4e-alert alert helpful exec-path-from-shell diminish keyfreq spaceline deft rainbow-delimiters git-auto-commit-mode magit which-key evil counsel ivy solarized-theme use-package))
+ '(show-paren-mode t)
+ '(smtpmail-smtp-server "smtp.web.de")
+ '(smtpmail-smtp-service 25)
+ '(tool-bar-mode nil))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  )
-(put 'narrow-to-region 'disabled nil)
